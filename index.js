8dff8a4d59a81d6532b55d4322b5468b3fadbb21
igoroizumi <igoroizumi2@gmail.com>
2025-09-25 10:07:13 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const core = require{'@actions/core'}
+
+try {
+  const meuInout = core.getInput{'my-input'}
+  console.log('Hello ${meuinput}')
+  core.setOutput("my-output", 'Hello ${meuInput}')
+} cath (error) {
+  core.setFailed('Ação falhou com erro: ${error.message} ')
+}
