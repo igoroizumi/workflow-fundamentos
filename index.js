438ea6de2fddfd8908fdc325f3653516773d150f
igoroizumi <igoroizumi2@gmail.com>
2025-09-25 10:14:28 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const core = require{'@actions/core'}
 try {
   const meuInout = core.getInput{'my-input'}
   console.log('Hello ${meuinput}')
-  core.setOutput("my-output", 'Hello ${meuInput}')
+  core.setOutput("my-output", 'Hello ${meuInput}!');
 } cath (error) {
-  core.setFailed('Ação falhou com erro: ${error.message} ')
+  core.setFailed('Ação falhou com erro: ${error.message}');
 }
